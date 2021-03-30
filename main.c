0ce0125ecbcb7e916b644afdb968816a0b1e9e7d
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2021-03-30 15:30:41 +0200

    Robenie demona

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/fs.h>
 int main(int argc, char** argv)
 {
     if(argc < 3) //argv[0] to nazwa ścieżka do .exe
@@ -14,7 +21,29 @@ int main(int argc, char** argv)
         //Sprawdzanie czy sciezka docelowa to katalog
         if (opendir(argv[2]))
         {
-            printf("Katalog docelowy istnieje, program -> demon\n");
+            pid_t pid;
+            int i;
+            /* stwórz nowy proces */
+            pid = fork( );
+            if (pid == -1)
+                return -1;
+            else if (pid != 0)
+                exit (EXIT_SUCCESS);
+            /* stwórz nową sesję i grupę procesów */
+            if (setsid( ) == -1)
+                return -1;
+            /* ustaw katalog roboczy na katalog główny */
+            if (chdir ("/") == -1)
+                return -1;
+            /* zamknij wszystkie pliki otwarte - użycie opcji NR_OPEN to przesada, lecz działa */
+            for (i = 0; i < NR_OPEN; i++)
+                close (i);
+            /* przeadresuj deskryptory plików 0, 1, 2 na /dev/null */
+            open ("/dev/null", O_RDWR); /* stdin */
+            dup (0); /* stdout */
+            dup (0); /* stderror */
+
+            return 0;
         }
         else if (ENOENT == errno)
         {
@@ -37,7 +66,7 @@ int main(int argc, char** argv)
         printf("opendir() sie wyjebal na plecy z jakiegos innego powodu\n");
         return -1;
     }
-
+/* tu należy wykonać czynności demona… */
 
 
 
