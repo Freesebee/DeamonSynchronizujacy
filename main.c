d7bf6b4749cd2b5fadebb8ce4d5b5e86403434a5
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2021-03-30 14:38:31 +0200

    Sprawdzanie czy sciezki sa katalogami v2

--- main.c
+++ main.c
@@ -11,29 +11,35 @@ int main(int argc, char** argv)
     //Sprawdzanie czy sciezka zrodlowa to katalog
     if (opendir(argv[0]))
     {
-        printf("Katalog zrodlowy istnieje, program -> demon");
+        //Sprawdzanie czy sciezka docelowa to katalog
+        if (opendir(argv[1]))
+        {
+            printf("Katalog docelowy istnieje, program -> demon");
+        }
+        else if (ENOENT == errno)
+        {
+            printf("Katalog docelowy nie istnieje");
+            return -1;
+        }
+        else
+        {
+            printf("opendir() sie wyjebal na plecy z jakiegos innego powodu");
+            return -1;
+        }
     }
     else if (ENOENT == errno)
     {
         printf("Katalog zrodlowy nie istnieje");
+        return -1;
     }
     else
     {
         printf("opendir() sie wyjebal na plecy z jakiegos innego powodu");
+        return -1;
     }
-    //Sprawdzanie czy sciezka docelowa to katalog
-    if (opendir(argv[1]))
-    {
-        printf("Katalog docelowy istnieje, program -> demon");
-    }
-    else if (ENOENT == errno)
-    {
-        printf("Katalog docelowy nie istnieje");
-    }
-    else
-    {
-        printf("opendir() sie wyjebal na plecy z jakiegos innego powodu");
-    }
+
+
+
 
 
     return 0;
