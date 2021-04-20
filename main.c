5a05f663ee089f16b7c1254bbaa9d4c3b1e85515
FreeseBee <ajaku@DESKTOP-JTEOCI7>
2021-04-20 15:20:00 +0200

    Przeniesienie sprawdzania argumentów do funkcji checkArguments()

--- main.c
+++ main.c
@@ -30,6 +30,8 @@ int isDirectory(const char *path) {
         return 0;
     return S_ISDIR(statbuffer.st_mode); //0 jesli NIE jest katalogiem
 }
+
+//Sprawdza czy plik o podanej  sciezce jest plikiem
 int isRegularFile(const char *path) {
     struct stat statbuffer;
     if (stat(path, &statbuffer) != 0) //success = 0
@@ -37,64 +39,70 @@ int isRegularFile(const char *path) {
     return S_ISREG(statbuffer.st_mode); //0 jesli NIE jest katalogiem
 }
 
-int main(int argc, char** argv)
+int checkArguments(int argc, char** argv)
 {
-    if(argc < 3) //argv[0] to nazwa ścieżka do .exe
+    //argv[0] to nazwa ścieżka do .exe
+    if(argc < 3)
     {
         printf("Program przyjmuje co najmniej 2 argumenty\n");
         return -1;
     }
-    //Sprawdzanie czy sciezka zrodlowa to katalog
-    if (opendir(argv[1]))
-    {
-        //Sprawdzanie czy sciezka docelowa to katalog
-        if (opendir(argv[2]))
-        {
-            pid_t pid;
-            int i;
-            /* stwórz nowy proces */
-            pid = fork();
-            if (pid == -1)
-                return -1;
-            else if (pid != 0)
-                exit (EXIT_SUCCESS);
-            /* stwórz nową sesję i grupę procesów */
-            if (setsid() == -1)
-                return -1;
-            /* ustaw katalog roboczy na katalog główny */
-            if (chdir ("/") == -1)
-                return -1;
-            /* zamknij wszystkie pliki otwarte - użycie opcji NR_OPEN to przesada, lecz działa */
-            for (i = 0; i < NR_OPEN; i++)
-                close (i);
-            /* przeadresuj deskryptory plików 0, 1, 2 na /dev/null */
-            open ("/dev/null", O_RDWR); /* stdin */
-            dup (0); /* stdout */
-            dup (0); /* stderror */
 
-            return 0;
-        }
-        else if (ENOENT == errno)
-        {
-            printf("Katalog docelowy nie istnieje\n");
-            return -1;
-        }
-        else
-        {
-            printf("opendir() sie wyjebal na plecy z jakiegos innego powodu\n");
-            return -1;
-        }
-    }
-    else if (ENOENT == errno)
+    //Sprawdzanie czy sciezka zrodlowa to katalog
+    if (isDirectory(argv[1]) != 0)
     {
-        printf("Katalog zrodlowy nie istnieje\n");
+        printf("Sciezka zrodlowa nie jest katalogiem\n");
         return -1;
     }
-    else
+//    else if (ENOENT == errno)
+//    {
+//        printf("Katalog zrodlowy nie istnieje\n");
+//        return -1;
+//    }
+
+    //Sprawdzanie czy sciezka docelowa to katalog
+    if (isDirectory(argv[2]) != 0)
     {
-        printf("opendir() sie wyjebal na plecy z jakiegos innego powodu\n");
+        printf("Sciezka docelowa nie jest katalogiem\n");
         return -1;
     }
+//    else if (ENOENT == errno)
+//    {
+//        printf("Katalog docelowy nie istnieje\n");
+//        return -1;
+//    }
+
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    if(checkArguments(argc, argv) != 0){
+        return -1;
+    }
+
+    pid_t pid;
+    int i;
+    /* stwórz nowy proces */
+    pid = fork();
+    if (pid == -1)
+        return -1;
+    else if (pid != 0)
+        exit (EXIT_SUCCESS);
+    /* stwórz nową sesję i grupę procesów */
+    if (setsid() == -1)
+        return -1;
+    /* ustaw katalog roboczy na katalog główny */
+    if (chdir ("/") == -1)
+        return -1;
+    /* zamknij wszystkie pliki otwarte - użycie opcji NR_OPEN to przesada, lecz działa */
+    for (i = 0; i < NR_OPEN; i++)
+        close (i);
+    /* przeadresuj deskryptory plików 0, 1, 2 na /dev/null */
+    open ("/dev/null", O_RDWR); /* stdin */
+    dup (0); /* stdout */
+    dup (0); /* stderror */
+
 /* tu należy wykonać czynności demona…  */
 
 
